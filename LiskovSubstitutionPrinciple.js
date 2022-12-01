a0d72f2ce41dd52008b5c8f4e88f04410cf5eae5
Mike Petrusha <petrushamike@gmail.com>
2022-12-01 18:32:04 +0300

    feat: add Liskov Substitution Principle file

---
+++ LiskovSubstitutionPrinciple.js
@@ -0,0 +1,67 @@
+/*
+class Bird {
+  fly() {
+    console.log("I can fly");
+  }
+}
+
+class Duck extends Bird {
+  quack() {
+    console.log("I can quack");
+  }
+}
+
+class Penguin extends Bird {
+  fly() {
+    throw new Error("Cannot fly");
+  }
+
+  swim() {
+    console.log("I can swim");
+  }
+}
+
+function makeBirdFly(bird) {
+  bird.fly();
+}
+
+const duck = new Duck();
+const penguin = new Penguin();
+
+makeBirdFly(duck);
+makeBirdFly(penguin);
+*/
+
+class FlyingBird {
+  fly() {
+    console.log("I can fly");
+  }
+}
+
+class SwimmingBird {
+  swim() {
+    console.log("I can swim");
+  }
+}
+
+class Duck extends FlyingBird {
+  quack() {
+    console.log("I can quack");
+  }
+}
+
+class Penguin extends SwimmingBird {}
+
+function makeFlyingBirdFly(bird) {
+  bird.fly();
+}
+
+function makeSwimmingBirdSwim(bird) {
+  bird.swim();
+}
+
+const duck = new Duck();
+const penguin = new Penguin();
+
+makeFlyingBirdFly(duck);
+makeSwimmingBirdSwim(penguin);
